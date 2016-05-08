63afd7f919e6f15226a4ed5fab6e7fa7383aa4b7
LeeLom <771704330@qq.com>
2016-05-08 11:28:05 +0800

    Initial Commit

---
+++ AVMusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVMusicPlayer
+//
+//  Created by LeeLom on 16/5/8.
+//  Copyright © 2016年 LeeLom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
